3a310a2a0fe9431693c4842600cde0fd4c37576d
anaiva <49117270+anaiva@users.noreply.github.com>
2019-06-11 08:52:10 +0200

    drugi x 11.06.

--- memory.js
+++ memory.js
@@ -1,25 +1,24 @@
 //dodajem boje u niz
 const bojeSve = ["crimson", "darkgreen", "blue", "orange", "yellow", "gray", "aqua", "violet", "lawngreen"];
-let n;
-let niz = [];
-let stil = "";
-let boje = [];
-//povezujem promenljivu sa svim div poljima
 let polja = document.querySelectorAll("section > div");
 polja = [...polja];
 let prozorKraj = document.getElementById("overlay");
-//pocetno vreme
-// const startTime = new Date().getTime();
+let tabla = document.getElementById("tabla");
 
-let klasa;
+let brojKartica;
+let niz = [];
+let stil = "";
+let boje = [];
+
+
+let klasa;  //za dodelu klase u zavisnosti od izbora velicine table
 let aktivnoPolje = "";//trenutno izabrano polje
 let izabranaPolja = [];//dva izabrna polja smestam u niz
 
-const brojParova = niz.length / 2;
 let brojacUparenih = 0;
-let brojacKlikova = 0;
+let brojacZaPar = 0;
 
-    const startTime = new Date().getTime();
+const startTime = new Date().getTime();
 
 
 //KLIK FUNKCIJA - za svako polje....
@@ -32,7 +31,7 @@ const klikFunkcija = function () {
 
     if (izabranaPolja.length === 0) {
         izabranaPolja[0] = aktivnoPolje; // otvoreno polje ubacujem u niz od 2 elementa 
-        brojacKlikova++;
+        brojacZaPar++;
         return;
 
     } else {
@@ -47,36 +46,30 @@ const klikFunkcija = function () {
                 //ostavlja klasu boje posto je pogodjen par             
                 niz = niz.filter(card => card.classList.contains("hidden"));
                 // provera da li su pogodjeni svi parovi
-                if (brojacUparenih === n / 2) {
+                if (brojacUparenih === brojKartica / 2) {
                     polja.forEach(card => card.classList.add("off"));
+
                     // kraj - overlay
                     function on() {
-                        // document.getElementById("overlay").style.display = "block";
                         prozorKraj.style.display = "block";
                         const endTime = new Date().getTime();
                         const gameTime = (endTime - startTime) / 1000
 
                         setTimeout(function () {
-                            document.getElementById("text").innerHTML = 
-                            "Broj poteza: " + brojacKlikova + '<br />' + " Vreme igre: " + gameTime + " sec" + '<br />' +'<br />'+ "Nova igra"
-                        }, 1500);
-                        // setTimeout(function () { location.reload(); }, 4000);
-                        prozorKraj.onclick = function offf() { location.reload() }
-
+                            document.getElementById("text").innerHTML =
+                                "Broj poteza: " + brojacZaPar + '<br />' + " Vreme igre: " + gameTime + " s" + '<br />' + '<br />' + "Nova igra"
+                        }, 1200);
+                        prozorKraj.onclick = function offf() { location.reload() };
                     }
-                on()
-                //     setTimeout (prozorKraj.onclick = function offf() { location.reload() }, 5000);
+                    on()
                 }
             } else {
-                izabranaPolja.forEach(card => card.classList.remove("rotacija"))
-                izabranaPolja.forEach(card => card.classList.add("rotacija"))
-                izabranaPolja.forEach(card => card.classList.add("hidden"))
-                izabranaPolja.forEach(card => card.classList.remove("rotacija"))
-
+                izabranaPolja.forEach(card => card.classList.remove("rotacija"));
+                izabranaPolja.forEach(card => card.classList.add("hidden"));
             }
             aktivnoPolje = ""; // reset
             izabranaPolja.length = 0; //prazan niz
-            niz.forEach(card => card.addEventListener("click", klikFunkcija))
+            niz.forEach(card => card.addEventListener("click", klikFunkcija));
 
         }, 1000)
     }
@@ -84,40 +77,35 @@ const klikFunkcija = function () {
 };
 
 //POCETNA
-//iskljucuje prikaz pocetne strane i pocinje da prikazuje tablu
 function funkcijaStart() {
-    
+
     let pocetak = document.getElementById("pocetnaStrana");
-    let sirinaPolja = ""; //sirina polja
-    let sekcija = document.getElementById("sirina");
-    // let prozorKraj = document.getElementById("overlay");
+    let sirinaPolja = ""; //sirina polja   
     prozorKraj.style.display = "none";
-
-    // polja.forEach(card => card.classList.add("start")); //tabla sa poljima je u startu sakrivena
-
     let klasa = document.getElementById("velicina").value; //sta je izabrano
+
     //odredjivanje velicine table za igru i dodavanje stila
     if (klasa == "S") {
-        n = 6;
+        brojKartica = 6;
         stil = "S";
         sirinaPolja = "32%";
-        sekcija.style.width = "60%";
-        sekcija.style.marginTop = "10%"
+        tabla.style.width = "60%";
+        tabla.style.marginTop = "10%"
 
     } else if (klasa == "M") {
-        n = 12;
+        brojKartica = 12;
         stil = "M";
-        sirinaPolja = "24%";
-        sekcija.style.width = "70%";
-        sekcija.style.marginTop = "2%";
+        sirinaPolja = "23%";
+        tabla.style.width = "70%";
+        tabla.style.marginTop = "2%";
 
     } else if (klasa == "L") {
-        n = 18;
+        brojKartica = 18;
         stil = "L";
         sirinaPolja = "15%";
-        sekcija.style.width = "90%";
-        sekcija.style.marginTop = "1%";
-        sekcija.style.marginLeft = "6%";
+        tabla.style.width = "96%";
+        tabla.style.marginTop = "1%";
+        tabla.style.marginLeft = "6%";
 
     }
     //otkrivanje table za igru
@@ -126,12 +114,12 @@ function funkcijaStart() {
 
     } else {
         pocetak.style.display = "none";
-        for (var i = 0; i < n; i++) {
+        for (var i = 0; i < brojKartica; i++) {
             niz[i] = polja[i];
             console.log(niz[i]);
         }
         //broj boje koje se dodele nizu u zavisnosti od velicine table
-        let brojBoja = n / 2;
+        let brojBoja = brojKartica / 2;
         for (let i = 0; i < brojBoja; i++) {
             boje[i] = bojeSve[i];
         }
@@ -158,7 +146,6 @@ function funkcijaStart() {
             })
             setTimeout(function () {
                 niz.forEach(card => {
-                    // card.style.width = sirinaPolja;
                     card.classList.add("hidden") //dodaje izgled pocetne table                    
                     // veza svakog polja sa klik funkciojom
                     card.addEventListener("click", klikFunkcija)
